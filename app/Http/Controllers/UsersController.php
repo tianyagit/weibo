a67a1bdd72e6c6d4450e0581d27cab626485a5f5
tianyamike <1974588993@qq.com>
2020-08-22 16:35:44 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function Create()
     {
         return view('users.create');
+    }
+
+    public function Show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
