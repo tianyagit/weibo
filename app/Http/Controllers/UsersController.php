2ae70e0da5a9cbe2dd82bb54b03165686c7f9ccb
tianyamike <1974588993@qq.com>
2020-08-25 11:30:16 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+//use Auth;
+use Illuminate\Support\Facades\Auth;
+
 class UsersController extends Controller
 {
     public function Create()
@@ -31,6 +34,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
     }
