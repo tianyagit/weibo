63da23fb82373c0ff7ffb457d4a41c8a90e4cf75
tianyamike <1974588993@qq.com>
2020-08-26 15:54:00 +0800

    用户微博列表

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Status;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Status::class)->times(100)->create();
+    }
+}
