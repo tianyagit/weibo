7e00932f60004b9b3f196a1eaa5bacc4ed4aac54
tianyamike <1974588993@qq.com>
2020-08-27 11:03:51 +0800

    关注按钮

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,7 +94,7 @@ class User extends Authenticatable
 
     public function isFollowing($user_id)
     {
-        $this->followings()->contains($user_id);
+        return $this->followings->contains($user_id);
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,6 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 
 Route::get('/user/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/user/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
